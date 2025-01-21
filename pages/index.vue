de084a304eef15645831b831f5cc323b040f48dc
alireza <farmanialireza668@gmail.com>
2025-01-21 12:51:10 +0330

    install tailwindcss and pini and start project

---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup>
+const message = "hrllo world this is home page"
+</script>
+
+<template>
+    <div>
+        <h2 class="text-center">{{ message }}</h2>
+    </div>
+</template>
