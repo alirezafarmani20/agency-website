e7c9ddd6b29e0ea8b9827ded4f908de21dbfb41f
alireza <farmanialireza668@gmail.com>
2025-01-21 12:58:25 +0330

    create main layouts

---
+++ layouts/Container.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="mx-3 sm:mx-7 md:mx-10">
+        <slot></slot>
+    </div>
+</template>
---
+++ store/main.ts
@@ -0,0 +1,5 @@
+import { defineStore } from "pinia";
+
+export const textStore = defineStore('useStore', {
+    //create my store here
+})
